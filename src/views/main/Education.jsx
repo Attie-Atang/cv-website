788eec3ee3b48ac26704e20567ae237d2f18ed2a
jassiel-ndlovu <nkosijassiel@gmail.com>
2024-10-01 22:23:46 +0200

    Changed dates in Education.jsx

--- src/views/main/Education.jsx
+++ src/views/main/Education.jsx
@@ -16,12 +16,12 @@ function Education() {
                     <tr>
                         <td>University of Excellence</td>
                         <td>Bachelor of Science in Computer Science</td>
-                        <td>2015 - 2019</td>
+                        <td>2022 - 2024</td>
                     </tr>
                     <tr>
                         <td>Kenilworth Secondary School</td>
                         <td>High School Diploma</td>
-                        <td>2010 - 2014</td>
+                        <td>2015 - 2021</td>
                     </tr>
                 </tbody>
             </table>
